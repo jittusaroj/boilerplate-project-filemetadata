a33203c16e1d0326665f25658a571889354d9d11
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-27 13:23:19 +0530

    commit file

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
 
+const upload = multer({ storage: storage });
+
+
+app.post("/api/fileanalyse",upload.single('upfile'), (req, res)=>{
+const fileDetails={
+  name:req.file.originalname,
+  type:req.file.mimetype,
+  size:req.file.size
+}
+res.json(fileDetails)
+})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -14,7 +34,7 @@ app.get('/', function (req, res) {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
